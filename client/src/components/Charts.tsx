be8ec0f2a2e0ae4a6a75e5e5ce563bc25fdcfb21
Guilhermelowa <guilhermelw.uefs@gmail.com>
2025-01-02 18:20:01 -0300

    Removes unused Charts.tsx

--- client/src/components/Charts.tsx
+++ client/src/components/Charts.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import { format } from 'date-fns';
-import { Bar, Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from 'chart.js';
-import { COLORS, CHART_OPTIONS } from '../styles/theme';
-import { AnalysisResponse } from '../types/analysis';
+import { Bar, Line, Scatter } from 'react-chartjs-2';
+import { 
+    Chart as ChartJS,
+    BarElement,
+    CategoryScale,
+    Legend,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip
+} from 'chart.js';
+import { MatrixController, MatrixElement } from 'chartjs-chart-matrix';
+import { COLORS, CHART_OPTIONS, HEATMAP_OPTIONS } from '../styles/theme';
+import { AnalysisResponse, HeatmapPoint } from '../types/apiTypes';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement);
+ChartJS.register(
+    BarElement,
+    CategoryScale,
+    Legend,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip
+);
 
 interface ChartProps {
   data: AnalysisResponse;
 }
 
 const Charts: React.FC<ChartProps> = ({ data }) => {
+    console.log(data.heatmap_data)
   const barData = {
     labels: Object.keys(data.word_metrics.messages_per_author),
     datasets: [
@@ -38,6 +59,24 @@ const Charts: React.FC<ChartProps> = ({ data }) => {
     ],
   };
 
+const scatterData = {
+    datasets: [{
+        label: 'Message Activity',
+        data: data.heatmap_data.map((point: HeatmapPoint) => ({
+            x: point.x,
+            y: point.y,
+            value: point.v
+        })),
+        backgroundColor: (context: any) => {
+            const value = context.raw?.value || 0;
+            const alpha = Math.min(value / 100, 1);
+            return `rgba(18, 140, 126, ${alpha})`;
+        },
+        pointStyle: 'rect',
+        pointRadius: 15,
+    }],
+};
+
   return (
     <div>
       <h2>Conversations</h2>
@@ -49,6 +88,8 @@ const Charts: React.FC<ChartProps> = ({ data }) => {
                 {format(new Date(data.conversation_stats.longest_conversation_end), 'PPP')}
             </p>
         </div>
+    <h2>Message Activity</h2>
+    <Scatter data={scatterData} options={HEATMAP_OPTIONS} />
       <h2>Messages per Author</h2>
       <Bar data={barData} options={CHART_OPTIONS} />
       <h2>Curse Words Frequency</h2>

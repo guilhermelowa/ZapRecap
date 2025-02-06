dcbd7c0f61bc626da3d5ffb4ac353513f16e2fb4
Guilhermelowa <guilhermelw.uefs@gmail.com>
2025-02-06 18:21:16 -0300

    Adds logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
+import recapLogo from '../assets/recap_logo.png';
 
 const Header: FC = () => {
   const { t } = useTranslation();
 
   return (
     <header className="whatsapp-header">
-      <h1>{t('title')}</h1>
+      <div className="header-content">
+        <img src={recapLogo} alt="Recap Logo" className="header-logo" />
+        <h1>{t('title')}</h1>
+      </div>
       <div className="language-switcher">
         <LanguageSwitcher />
       </div>

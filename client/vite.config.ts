62ffb0154dede1c29ee792325de637d82f0ead99
Guilhermelowa <guilhermelw.uefs@gmail.com>
2025-02-11 17:20:26 -0300

    Removes viteconfigPaths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   define: {
     __WS_TOKEN__: JSON.stringify(process.env.WS_TOKEN || 'development'),
     global: {}

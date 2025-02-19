9afda2f35a12877214bcc53e6b062b122c24b343
Guilhermelowa <guilhermelw.uefs@gmail.com>
2025-02-18 21:46:59 -0300

    Fixes API requests url

--- client/src/services/axiosConfig.ts
+++ client/src/services/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: __API_BASE_URL__,
+  baseURL: import.meta.env.VITE_NODE_ENV === 'production' 
+    ? 'https://zap-recap-ffe516b006a4.herokuapp.com/' 
+    : 'http://localhost:8000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
